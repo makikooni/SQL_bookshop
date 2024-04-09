b35c992093d4f237d37752117cafaf6e4493a3a5
Chun Ling <92478263+dondon199712@users.noreply.github.com>
2024-04-09 12:55:24 +0200

    Insert into genres (new)

--- Elephant.session.sql
+++ Elephant.session.sql
@@ -1,31 +1,42 @@
-DROP TABLE IF EXISTS BOOKS;
+DROP TABLE IF EXISTS books;
 
-CREATE TABLE BOOKS (
+
+CREATE TABLE books (
     book_id INT PRIMARY KEY NOT NULL, 
-    title VARCHAR(80),
+    title VARCHAR(80) NOT NULL,
     price_in_pence INT NOT NULL,
-    quantity_in_stock INT NOT NULL,
+    quantity_in_stock INT DEFAULT 0 NOT NULL,
     release_date DATE,
-    is_fiction BOOLEAN);
+    is_fiction BOOLEAN
+    );
 
 
-INSERT INTO BOOKS
+INSERT INTO books
 (book_id, title, price_in_pence, quantity_in_stock, release_date, is_fiction)
 VALUES
-  (1, 'The Hitchhiker''s Guide to the Galaxy', 1995, 5, '1979-09-21', TRUE),
-  (2, 'Pride & Prejudice', 1295, 10, '1813-01-28', FALSE),
-  (3, 'The Lord of the Rings: The Fellowship of the Ring', 2499, 3, '1954-07-29', FALSE),
-  (4, 'To Kill a Mockingbird', 1495, 7, '1960-07-11', FALSE),
-  (5, 'One Hundred Years of Solitude', 1795, 8, '1967-06-05', FALSE),
-  (6, 'Frankenstein', 1699, 2, '1818-01-01', FALSE),
-  (7, 'Dune', 1895, 4, '1965-08-01', FALSE),
-  (8, 'The Handmaid''s Tale', 1399, 9, '1985-04-30', FALSE),
-  (9, 'The Great Gatsby', 1195, 6, '1925-04-10', FALSE),
-  (10, 'Moby Dick', 2199, 1, '1851-11-14', FALSE);
+  (1,'The Hitchhiker''s Guide to the Galaxy', 899, 560, '1997-10-12', true),
+(2,'The Little Prince',699,1020,'1943-04-06',true ),
+(3,'The Tale of Peter Rabbit',599,1000,'1902-10-01',true),
+(4,'Emma', 522,390,'1815-12-23',true),
+(5,'Nineteen Eighty-Four: A Novel',799,420,'1949-06-08',true),
+(6,'The Handmaid''s Tale',899,10,'1985-08-01', true),
+(7,'The War of the Worlds',250,17,'1897-04-01',true),
+(8,'Captain Corelli''s Mandolin',999,0,'1995-08-29',true),
+(9,'A Brief History of Time',825,0,'1988-04-01',false),
+(10,'Pride and Prejudice',699,4,'1813-01-28',true);
+
 
 SELECT *
 FROM books
 WHERE is_fiction IS FALSE;
+
+SELECT *
+FROM books
+WHERE release_date BETWEEN '1800-01-01' AND '1999-12-31';
+
+SELECT *
+FROM books
+WHERE quantity_in_stock >= 1;
 
 UPDATE books
 SET quantity_in_stock = 0
@@ -34,25 +45,78 @@ WHERE book_id = 4;
 DELETE FROM BOOKS
 WHERE quantity_in_stock = 0;
 
+DROP TABLE IF EXISTS authors;
 
 CREATE TABLE authors(
   author_id INT PRIMARY KEY,
   author_name VARCHAR(200),
-  fun_fact VARCHAR(100)
+  fun_fact TEXT
 );
 
-
-INSERT INTO authors (author_id, author_name, fun_fact)
+INSERT INTO authors (
+author_id, author_name, fun_fact)
 VALUES
-    (1, 'Douglas Adams', 'Scriptwriter and radio dramatist.'),
-    (2, 'Jane Austen', 'Explored women''s dependence on marriage.'),
-    (3, 'J.R.R. Tolkien', 'Created languages for his worlds.'),
-    (4, 'Harper Lee', 'Won Pulitzer Prize for "To Kill a Mockingbird."'),
-    (5, 'Gabriel Garcia Marquez', 'Nobel Prize in Literature winner.'),
-    (6, 'Mary Shelley', 'Wrote "Frankenstein" at age 18.'),
-    (7, 'Frank Herbert', 'Wrote "Dune," a classic sci-fi novel.'),
-    (8, 'Margaret Atwood', 'Known for dystopian novels.'),
-    (9, 'F. Scott Fitzgerald', 'Coined the term "Jazz Age."'),
-    (10, 'Herman Melville', 'Drew on sailor experience for "Moby Dick."');
+(1,'Dan Brown','Favourite colour is not brown.'),
+(2,'Antoine de Saint-Exupéry','He was a successful commercial pilot before World War II, working airmail routes in Europe, Africa, and South America.'),
+(3,'Douglas Adams','He made two appearances in Monty Python''s Flying Circus.'),
+(4,'Stephen Hawking','Doctors told him he wouldn''t live past his early 20s.'),
+(5,'Eric Carle','When he was a young boy, Carle had a dream that he would build a bridge from Germany to America.'),
+(6,'J.D. Salinger','The Catcher in the Rye was the only novel that J.D. Salinger published during his lifetime - not bad for a first try!'),
+(7,'Beatrix Potter','Between 1881 and 1897 Potter kept a journal in which she jotted down her private thoughts in a secret code . This code was so fiendishly difficult it was not cracked and translated until 1958.'),
+(8,'C.S.Lewis','Lewis set up a charitable trust to give away whatever money he received from his books.'),
+(9,'Roald Dahl','During World War II he passed intelligence to MI6 from Washington.'),
+(10,'Frank Herbert','While conversing with fungi expert Paul Stamets, Herbert revealed that the world of Dune was influenced by the lifecycle of mushrooms, with his imagination being helped along by a more "magic" variety.'),
+(11,'Louis de Bernières','De Bernières is an avid musician who plays flute, mandolin, clarinet and guitar.'),
+(12,'H.G.Wells','In 1914 H.G. Wells published a novel titled The World Set Free. In this book he described a weapon that was eerily similar to the first atomic bomb unleashed on the Japanese cities of Hiroshima and Nagasaki in 1945.'),
+(13,'George Orwell','Orwell intentionally got himself arrested for being "drunk and incapable".'),
+(14,'Jane Austen','The author of her first novel, Sense and Sensibility, was simply "A Lady", and her later works like Pride and Prejudice were credited to "the Author of Sense and Sensibility". She wasn''t named as the author of her novels until after her death!'),
+(15,'Margaret Atwood','Atwood was the first author to contribute to The Future Library Project, which will take one writer''s contribution each year for one hundred years to be printed in the year 2114.');
 
 
+
+ALTER TABLE books
+ADD COLUMN author_id INT;
+
+ALTER TABLE books
+ADD COLUMN author_name VARCHAR (200);
+
+UPDATE books
+SET author_id = 3
+WHERE book_id = 1;
+
+UPDATE books
+SET author_id = 
+    CASE 
+        WHEN book_id = 2 THEN 2
+        WHEN book_id = 4 THEN 14
+        WHEN book_id = 3 THEN 7
+        WHEN book_id = 5 THEN 13
+        WHEN book_id = 6 THEN 15
+        WHEN book_id = 7 THEN 12
+        WHEN book_id = 8 THEN 11
+        WHEN book_id = 9 THEN 4
+        WHEN book_id = 10 THEN 14
+        ELSE author_id 
+    END;
+
+SELECT *
+FROM books b
+INNER JOIN authors a
+ON b.author_id = a.author_id
+ORDER BY book_id ASC;
+
+CREATE TABLE genres (
+genre_id INT PRIMARY KEY NOT NULL,
+genre_name VARCHAR(50)
+);
+
+INSERT INTO genres (genre_id, genre_name)
+VALUES
+(1, 'science fiction'),
+(2, 'children''s'),
+(3, 'romance'),
+(4, 'fantasy'),
+(5, 'dystopian'),
+(6, 'science'),
+(7, 'adventure'),
+(8, 'classics');
